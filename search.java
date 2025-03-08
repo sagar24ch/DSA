b430185ad95edca9ba8622b4e6c7da51f8e70f9d
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2025-03-08 23:12:58 +0530

    Add files via upload

---
+++ search.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+    public class search
+{
+    public static void main(String[] args)
+        {
+            Scanner obj = new Scanner(System.in);
+            int [] elemennts = {21,25,30,34,37,40,44,48};
+            System.out.println("enter element to search");
+        }
+}
