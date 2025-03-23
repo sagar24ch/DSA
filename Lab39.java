5c3cea366922c49fb6e0ca094327c3374ba04398
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2025-03-23 23:26:50 +0530

    Add files via upload

---
+++ Lab39.java
@@ -0,0 +1,14 @@
+public class Lab39 {
+    public static void main(String as[]){
+        int row=5;
+        int star=1;
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=star;j++){
+                System.out.print(" * ");
+            }
+        System.out.println("");
+        star=star+1;
+        }
+        
+    }
+}
