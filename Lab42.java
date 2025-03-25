ee522071701f4232ff79478911af06582c5084e4
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2025-03-25 23:44:17 +0530

    Add files via upload

---
+++ Lab42.java
@@ -0,0 +1,21 @@
+public class Lab42 {
+    public static void main(String as[]){
+   /*  int a=12;
+    int b=23;
+    int result=a&b;
+    System.out.println(result);
+    boolean c=true;
+    boolean d=false;
+    System.out.println(c&d);
+    }
+    */
+    int a=12;
+    int b=23;
+    int result=a|b;
+    System.out.println(result);
+
+    boolean c=false;
+    boolean d=false;
+    System.out.println(c|d);
+    }
+}
