a2f02601ee9a3eb3affe46f3b3738e3c894283f5
Sagar Gorai <151768071+sagar24ch@users.noreply.github.com>
2025-04-09 23:04:41 +0530

    Add files via upload

---
+++ Lab44.java
@@ -0,0 +1,12 @@
+public class Lab44 {
+    public static void main(String []as){
+        int n=17;
+        if((n&1)==1){
+            System.out.println("Number is odd");
+        }
+        else{
+            System.out.println("Number is even");
+        }
+    }
+    
+}
